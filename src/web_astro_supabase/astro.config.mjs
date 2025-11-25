08aaaa5c616f19da1a9c8df9a8d3181be52d2118
Katie Kodes <44843658+kkgthb@users.noreply.github.com>
2025-11-25 08:52:52 -0600

    Added Vercel as a host (#2)

--- src/web_astro_supabase/astro.config.mjs
+++ src/web_astro_supabase/astro.config.mjs
@@ -1,10 +1,13 @@
 /// <reference types="astro/client" />
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import vercelServerless from '@astrojs/vercel/serverless';
+
+const adapter = process.env.VERCEL
+  ? vercelServerless()
+  : node({ mode: "standalone" });
 
 export default defineConfig({
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter,
   output: "server",
 });
