987a6e8e189fbc2769529398c74a34e935f64b78
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-11-25 08:48:26 -0600

    Only use Vercel adapter if actually within Vercel

--- src/web_astro_supabase/astro.config.mjs
+++ src/web_astro_supabase/astro.config.mjs
@@ -1,8 +1,13 @@
 /// <reference types="astro/client" />
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import vercelServerless from '@astrojs/vercel/serverless';
 
+const adapter = process.env.VERCEL
+  ? vercelServerless()
+  : node({ mode: "standalone" });
+
 export default defineConfig({
-  adapter: vercelServerless(),
+  adapter,
   output: "server",
 });
